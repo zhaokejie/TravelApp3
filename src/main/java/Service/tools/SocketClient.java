834c58170c8f05fe6f417bc8c585743b96048c52
zhaokejie <870262949@qq.com>
2021-05-20 16:35:15 +0800

    socket意外关闭问题1.4

--- src/main/java/Service/tools/SocketClient.java
+++ src/main/java/Service/tools/SocketClient.java
@@ -12,72 +12,77 @@ import java.net.UnknownHostException;
 
 public class SocketClient {
 
-    static Socket socket;
-    static InputStream is = null;
-    static BufferedReader in = null;
-    static OutputStream os = null;
-
-    static
-    {
-        try {socket = new Socket("127.0.0.1",50006);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
 
 
-    public static void main(String args[])throws Exception {
-
-        try {
-
-            //获取输出流，向服务器端发送信息
-            OutputStream os=socket.getOutputStream();//字节输出流
-            PrintWriter pw=new PrintWriter(os);//将输出流包装为打印流
-            pw.write("我是Java服务器");
-            pw.flush();
-            socket.shutdownOutput();//关闭输出流
-
-            InputStream is=socket.getInputStream();
-            BufferedReader in = new BufferedReader(new InputStreamReader(is));
-            String info=null;
-            while((info=in.readLine())!=null){
-                System.out.println("我是客户端，Python服务器说："+info);
-            }
-            is.close();
-            in.close();
-            socket.close();
-        } catch (UnknownHostException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    public static void closeSocket()
-    {
-        try {
-            socket.shutdownOutput();//关闭输出流
-            socket.close();
-            is.close();
-            in.close();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//    static
+//    {
+//        try {);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
 
 
-    }
+//    public static void main(String args[])throws Exception {
+//
+//        try {
+//
+//            //获取输出流，向服务器端发送信息
+//            OutputStream os=socket.getOutputStream();//字节输出流
+//            PrintWriter pw=new PrintWriter(os);//将输出流包装为打印流
+//            pw.write("我是Java服务器");
+//            pw.flush();
+//            socket.shutdownOutput();//关闭输出流
+//
+//            InputStream is=socket.getInputStream();
+//            BufferedReader in = new BufferedReader(new InputStreamReader(is));
+//            String info=null;
+//            while((info=in.readLine())!=null){
+//                System.out.println("我是客户端，Python服务器说："+info);
+//            }
+//            is.close();
+//            in.close();
+//            socket.close();
+//        } catch (UnknownHostException e) {
+//            // TODO Auto-generated catch block
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//    public static void closeSocket()
+//    {
+//        try {
+//            socket.shutdownOutput();//关闭输出流
+//            socket.close();
+//            is.close();
+//            in.close();
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+
+    //}
     public static void sendMessage(String mess)
     {
         try {
+
+            Socket socket;
+            socket = new Socket("127.0.0.1",50006);
+            InputStream is = null;
+            BufferedReader in = null;
+            OutputStream os = null;
+            PrintWriter pw = null;
 
             System.out.println("------------SocketClient----------");
             System.out.println(mess);
             //获取输出流，向服务器端发送信息
             if(os == null)
                os =socket.getOutputStream();//字节输出流
-            PrintWriter pw=new PrintWriter(os);//将输出流包装为打印流
+            if(pw != null)
+            pw = new PrintWriter(os);//将输出流包装为打印流
             pw.write(mess);
             pw.flush();
 
